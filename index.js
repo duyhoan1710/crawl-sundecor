d966973bdd0d085fc40db0b7049fa0e91e988dd7
Thanh Tam <tamntt2710@gmail.com>
2021-12-27 10:17:32 +0700

    crawl_v2

--- index.js
+++ index.js
@@ -5,9 +5,111 @@ const url = 'https://sundecor.vn';
 
 [
   {
-    url: 'https://sundecor.vn/den-chum-pha-le-ban-chay.html',
-    subcategory_id: 1,
-  }
+    url: 'https://sundecor.vn/quat-tran-den-canh-go.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-nhat-ban.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-den-asia.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-my.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-nhap-khau-fanco.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-den-co-dien.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-canh-cup-canh-xoe.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-panasonic.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/quat-tran-dien-co.html',
+    subcategory_id: 5,
+  },
+  {
+    url: 'https://sundecor.vn/den-ban-serip-dong.html',
+    subcategory_id: 6,
+  },
+  {
+    url: 'https://sundecor.vn/den-ban-hien-dai.html',
+    subcategory_id: 6,
+  },
+  {
+    url: 'https://sundecor.vn/den-ban-co-dien.html',
+    subcategory_id: 6,
+  },
+  {
+    url: 'https://sundecor.vn/den-san-hien-dai.html',
+    subcategory_id: 6,
+  },
+  {
+    url: 'https://sundecor.vn/den-tha-hien-dai.html',
+    subcategory_id: 7,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-pha-le.html',
+    subcategory_id: 7,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-dong.html',
+    subcategory_id: 7,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-pha-le.html',
+    subcategory_id: 7,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-hien-dai.html',
+    subcategory_id: 7,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-co-dien.html',
+    subcategory_id: 7,
+  },
+  {
+    url: 'https://sundecor.vn/den-soi-tranh-dong.html',
+    subcategory_id: 7,
+  },
+
+  // Đèn tường
+  {
+    url: 'https://sundecor.vn/den-tuong-pha-le.html',
+    subcategory_id: 8,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-dong.html',
+    subcategory_id: 8,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-hien-dai.html',
+    subcategory_id: 8,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-co-dien.html',
+    subcategory_id: 8,
+  },
+  {
+    url: 'https://sundecor.vn/den-tuong-co-dien.html',
+    subcategory_id: 8,
+  },
+  {
+    url: 'https://sundecor.vn/den-soi-tranh-dong.html',
+    subcategory_id: 8,
+  },
 ].forEach((item) => {
   axios(item.url).then((response) => {
     const $ = cheerio.load(response.data);
@@ -29,7 +131,7 @@ const url = 'https://sundecor.vn';
         cloneProject.content = productHTML('#dp1').html();
         cloneProject.image_1 = `${url}${productHTML('.pb .ll .t')[0].attribs.href}`;
         cloneProject.name = productHTML('.pb .rr h1').text();
-        cloneProject.code = productHTML('.pb .rr .cc li')[0].children[0].data;
+        cloneProject.code = productHTML('.pb .rr .cc li')[0].children[0].data.replace('Mã SP: ','');
         cloneProject.size = productHTML('.pb .rr .cc li')[1].children[0].data.replace('Kích thước: ', '');
         cloneProject.guarantee = productHTML('.pb .rr .cc li')[2].children[0].data.replace('Bảo hành: ', '');
         cloneProject.status = productHTML('.pb .rr .cc li')[3].children[0].data.replace('Tình trạng: ', '');
